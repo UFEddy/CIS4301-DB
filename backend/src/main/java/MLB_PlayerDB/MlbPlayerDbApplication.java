c2c90d503b77b44dd7176923a90cb44612ebd963
CVelez21 <80861411+CVelez21@users.noreply.github.com>
2024-11-03 23:25:35 -0500

    Creation of Backend and new Setup script
    This update include the addition of code infrastructure for the backend in Java setup with dependencies for Oracle but currently configured to use H2 database. A new setup script is also added that checks for necessary items needed for the backend to work with options for users to install the missing ones if needed

---
+++ backend/src/main/java/MLB_PlayerDB/MlbPlayerDbApplication.java
@@ -0,0 +1,13 @@
+package MLB_PlayerDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlbPlayerDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlbPlayerDbApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/MLB_PlayerDB/MlbPlayerDbApplicationTests.java
@@ -0,0 +1,13 @@
+package MLB_PlayerDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlbPlayerDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
