8bffdac8691f248031eafce2e3295943124a3da9
eddyrosales <eddy.rosales@ufl.edu>
2024-12-05 21:01:57 -0500

    Add button to return tuple count. Add styles

--- frontend/src/Welcome.js
+++ frontend/src/Welcome.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './App.css';
 
 function Welcome() {
   const navigate = useNavigate();
+
+  const [totalRows, setTotalRows] = useState(null);
+  const [error, setError] = useState(null);
 
   const handleLogout = () => {
     localStorage.removeItem('token');
@@ -19,22 +22,52 @@ function Welcome() {
     navigate('/cost-of-war'); // Navigate to the Cost of WAR page
   };
 
+  const getTuples = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/count');
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+      setTotalRows(data.total_rows);
+      setError(null);
+
+    } catch(err) {
+      setError(err.message);
+      setTotalRows(null);
+    }
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <h2>MLB Player Evaluation Database</h2>
         <p>You have successfully logged in!</p>
+
         <button className="App-button" onClick={handleLogout}>
           Logout
         </button>
+
         {/* Temporary login bypass button */}
         <button className="App-button" onClick={handleTestAccess}>
           Test Access
         </button>
+
         <button className="App-button" onClick={goToCostOfWAR}>
           View Cost of WAR
         </button>
+
+        {/* Add a button to get tuples */}
+
+        <button className="App-button" onClick={getTuples}>
+          Count Tuples
+        </button>
+        {/* Display the result or error */}
+        {totalRows !== null && <p>Total Rows: {totalRows}</p>}
+        {error && <p style={{ color: 'red' }}>Error: {error}</p>}
+
       </header>
     </div>
   );
