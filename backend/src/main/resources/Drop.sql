9114aab17b16119d1805fe01c5e2dc5ea406bca4
TSyphon-98 <nmctear@ufl.edu>
2024-12-07 11:51:28 -0500

    Deleted redundant Drop.SQL

--- backend/src/main/resources/Drop.sql
+++
@@ -1,6 +0,0 @@
-BEGIN
-    FOR table_name IN (SELECT table_name FROM user_tables) LOOP
-        EXECUTE IMMEDIATE 'DROP TABLE ' || table_name.table_name || ' CASCADE CONSTRAINTS';
-    END LOOP;
-END;
-/
