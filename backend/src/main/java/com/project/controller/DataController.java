abde851a68ce3e805ea1b5f0d23de6a5c5387255
Jonas Kazimli <jonaskazimli0@gmail.com>
2024-12-06 22:29:50 -0500

    add query2 endpoint

--- backend/src/main/java/com/project/controller/DataController.java
+++ backend/src/main/java/com/project/controller/DataController.java
@@ -1,27 +1,47 @@
 package com.project.controller;
 
+import org.springframework.web.bind.annotation.*;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@RestController
+@RequestMapping("/api")
 public class DataController {
-    public static void main(String[] args) throws SQLException {
-        List<Map<String, String>> data = getData("SELECT * from player");
-        System.out.println(data);
-        for (Map<String, String> map : data){
-            System.out.println(map.toString());
-        }
+    public static void main(String[] args) {
+        String query = "SELECT " +
+                "    g.SeasonYear AS Year, " +
+                "    CASE " +
+                "        WHEN g.HomeTeamID = ps.TeamID THEN 'Home' " +
+                "        WHEN g.AwayTeamID = ps.TeamID THEN 'Away' " +
+                "    END AS GameType, " +
+                "    AVG(pg.WAR) AS AverageWAR " +
+                "FROM " +
+                "    PlayerGame pg " +
+                "JOIN " +
+                "    Game g ON pg.GameID = g.GameID " +
+                "JOIN " +
+                "    PlayerSeason ps ON pg.PlayerID = ps.PlayerID " +
+                "    AND g.SeasonYear = ps.SeasonYear " +
+                "WHERE g.SeasonYear = '2023' " +
+                "GROUP BY " +
+                "    g.SeasonYear, " +
+                "    CASE " +
+                "        WHEN g.HomeTeamID = ps.TeamID THEN 'Home' " +
+                "        WHEN g.AwayTeamID = ps.TeamID THEN 'Away' " +
+                "    END " +
+                "ORDER BY " +
+                "    g.SeasonYear, GameType";
+        System.out.println(getData(query));
     }
-
     public static List<Map<String, String>> getData(String query) {
         List<Map<String, String>> data = new ArrayList<>();
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@oracle.cise.ufl.edu:1521:orcl", "YKAZIMLI",
-                    "4vReeT5XtaKNPnc633rMNLeF");
+                    "jdbc:oracle:thin:@oracle.cise.ufl.edu:1521:orcl", "YKAZIMLI", "4vReeT5XtaKNPnc633rMNLeF");
             Statement statement =
                     connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
@@ -45,5 +65,35 @@ public class DataController {
         }
 
         return data;
+    }
+
+    @PostMapping("/query2")
+    public List<Map<String, String>> getQuery2Results(@RequestBody Map<String, String> request) {
+        String seasonYear = request.get("seasonYear");
+        String query = "SELECT " +
+                "    g.SeasonYear AS Year, " +
+                "    CASE " +
+                "        WHEN g.HomeTeamID = ps.TeamID THEN 'Home' " +
+                "        WHEN g.AwayTeamID = ps.TeamID THEN 'Away' " +
+                "    END AS GameType, " +
+                "    AVG(pg.WAR) AS AverageWAR " +
+                "FROM " +
+                "    PlayerGame pg " +
+                "JOIN " +
+                "    Game g ON pg.GameID = g.GameID " +
+                "JOIN " +
+                "    PlayerSeason ps ON pg.PlayerID = ps.PlayerID " +
+                "    AND g.SeasonYear = ps.SeasonYear " +
+                "WHERE g.SeasonYear = '" + seasonYear + "' " +
+                "GROUP BY " +
+                "    g.SeasonYear, " +
+                "    CASE " +
+                "        WHEN g.HomeTeamID = ps.TeamID THEN 'Home' " +
+                "        WHEN g.AwayTeamID = ps.TeamID THEN 'Away' " +
+                "    END " +
+                "ORDER BY " +
+                "    g.SeasonYear, GameType";
+
+        return getData(query);
     }
 }
