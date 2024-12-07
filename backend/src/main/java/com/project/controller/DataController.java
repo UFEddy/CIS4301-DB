9b047c30e1eedc5852dbff90cc17404301bb1bf3
Jonas Kazimli <jonaskazimli0@gmail.com>
2024-12-06 21:24:40 -0500

    fix iteration issue

--- backend/src/main/java/com/project/controller/DataController.java
+++ backend/src/main/java/com/project/controller/DataController.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class DataController {
     public static void main(String[] args) throws SQLException {
-        List<Map<String, String>> data = getData("SELECT * FROM airport");
+        List<Map<String, String>> data = getData("SELECT * from player");
         System.out.println(data);
         for (Map<String, String> map : data){
             System.out.println(map.toString());
@@ -20,18 +20,17 @@ public class DataController {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@oracle.cise.ufl.edu:1521:orcl", "ykazimli",
-                    "<PASSWORD>");
+                    "jdbc:oracle:thin:@oracle.cise.ufl.edu:1521:orcl", "YKAZIMLI",
+                    "4vReeT5XtaKNPnc633rMNLeF");
             Statement statement =
                     connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
             ResultSet resultSet = statement.executeQuery(query);
             ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
-
             while (resultSet.next()) {
                 Map<String, String> map = new HashMap<>();
 
-                for (int columnNumber = 1; columnNumber < resultSetMetaData.getColumnCount(); columnNumber++) {
+                for (int columnNumber = 1; columnNumber <= resultSetMetaData.getColumnCount(); columnNumber++) {
                     map.put(resultSetMetaData.getColumnName(columnNumber),
                             resultSet.getString(resultSetMetaData.getColumnName(columnNumber)));
                 }
